5bac9293b66d701af68dacc6dc929ae4a64b3f19
Kevin Brent <kevin@kevinbrent.com>
2020-07-12 11:30:40 -0700

    bug - namespce for woocommerce class

--- wp-data-sync.php
+++ wp-data-sync.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Data Sync
  * Plugin URI:  https://wpdatasync.com/products/
  * Description: Sync raw data from WP Data Sync API to your WordPress website
- * Version:     1.1.0
+ * Version:     1.1.1
  * Author:      WP Data Sync
  * Author URI:  https://wpdatasync.com
  * License:     GPL2
@@ -22,7 +22,7 @@ namespace WP_DataSync;
 $uploads = wp_get_upload_dir();
 
 $defines = [
-	'WP_DATA_SYNC_VERSION' => '1.1.0',
+	'WP_DATA_SYNC_VERSION' => '1.1.1',
 	'WP_DATA_SYNC_CAP'     => 'manage_options',
 	'WP_DATA_SYNC_PLUGIN'  => plugin_basename( __FILE__ ),
 	'WP_DATA_SYNC_VIEWS'   => plugin_dir_path( __FILE__ ) . 'views/',
@@ -56,11 +56,7 @@ add_action( 'plugins_loaded', function() {
 		 */
 
 		add_action( 'wp_data_sync_after_process', function ( $post_id, $data, $data_sync ) {
-
-			if ( class_exists( 'WooCommerce', FALSE ) ) {
-				WC_DataSync::instance( $data_sync )->wc_process( $post_id, $data );
-			}
-
+			App\WC_DataSync::instance( $data_sync )->wc_process( $post_id, $data );
 		}, 10, 3 );
 
 	}
