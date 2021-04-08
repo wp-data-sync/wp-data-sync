fb4f5b3ffad5dd4714ef8f8be4048c7043912a9a
Kevin Brent <kevin@kevinbrent.com>
2021-04-08 08:59:41 -0700

    Add affiliate links to help buttons

--- views/settings/help-buttons.php
+++ views/settings/help-buttons.php
@@ -24,15 +24,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div class="wpds-help-buttons">
 
-	<a href="https://wpdatasync.com/products/" class="button-primary" target="_blank">
+	<a href="https://wpdatasync.com/products/?affid=admin" class="button-primary" target="_blank">
 		<?php _e( 'Products', 'wp-data-sync' ); ?>
 	</a>
 
-	<a href="https://wpdatasync.com/support/" class="button-primary" target="_blank">
+	<a href="https://wpdatasync.com/support/?affid=admin" class="button-primary" target="_blank">
 		<?php _e( 'Support', 'wp-data-sync' ); ?>
 	</a>
 
-	<a href="https://wpdatasync.com/docunentation/" class="button-primary" target="_blank">
+	<a href="https://wpdatasync.com/docunentation/?affid=admin" class="button-primary" target="_blank">
 		<?php _e( 'Documentation', 'wp-data-sync' ); ?>
 	</a>
 
